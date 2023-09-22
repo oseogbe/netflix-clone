ae0be6520313ed8a0220671c21bc9136a8c7bbdd
Ose Ogbe <therealnylat@gmail.com>
2023-09-22 02:05:01 +0100

    site header

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "rb.gy",
+                port: "",
+                pathname: "/ulxxee/**",
+            },
+        ],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
