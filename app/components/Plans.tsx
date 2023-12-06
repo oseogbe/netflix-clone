e51c3cf39af8499ceb2ae4e50d14920063ce5a71
Ose Ogbe <therealnylat@gmail.com>
2023-12-07 00:36:47 +0100

    integrating stripe for plan subscription

---
+++ app/components/Plans.tsx
@@ -0,0 +1,67 @@
+import useAuth from "@/hooks/useAuth"
+import { CheckIcon } from "@heroicons/react/20/solid"
+import type { Metadata } from "next"
+import Image from "next/image"
+import Link from "next/link"
+
+export const metadata: Metadata = {
+    title: 'Netflixx',
+    icons: [{ rel: 'icon', url: 'netflixx-icon.png' }],
+}
+
+const Plans = () => {
+    const { logout } = useAuth()
+
+    return (
+        <div>
+            <header>
+                <Link href="/">
+                    <Image
+                        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+                        alt="netflixx logo"
+                        width={150}
+                        height={90}
+                        className="cursor-pointer object-contain"
+                    />
+                </Link>
+
+                <button className="text-lg font-medium hover:underline" onClick={logout}>Sign Out</button>
+            </header>
+
+            <main className="max-w-5xl px-5 pt-28 pb-12 transition-all md:px-10">
+                <h1 className="mb-3 text-3xl font-medium">Choose the plan that's right for you</h1>
+                <ul>
+                    <li className="flex items-center gap-x-2 text-lg">
+                        <CheckIcon className="w-7 h-7 text-[#E50914]" /> Watch all you want. Ad-free.
+                    </li>
+                    <li className="flex items-center gap-x-2 text-lg">
+                        <CheckIcon className="w-7 h-7 text-[#E50914]" /> Recommendations just for you.
+                    </li>
+                    <li className="flex items-center gap-x-2 text-lg">
+                        <CheckIcon className="w-7 h-7 text-[#E50914]" /> Change or cancel your plan anytime.
+                    </li>
+                </ul>
+
+                <div className="mt-4 flex flex-col space-y-4">
+                    <div className="flex w-full items-center justify-end self-end md:w-3/5">
+                        <div className="planBox">
+                            standard
+                        </div>
+                        <div className="planBox">
+                            standard
+                        </div>
+                        <div className="planBox">
+                            standard
+                        </div>
+                    </div>
+
+                    {/* <Table /> */}
+
+                    <button>Subscribe</button>
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default Plans
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,95 @@
 "use client"
 
-import { RecoilRoot } from 'recoil'
-import PageContent from './content'
+import { RecoilRoot, useRecoilValue } from 'recoil'
+import { modalState } from '@/atoms/modalAtom'
+import Header from './components/Header';
+import Banner from './components/Banner';
+import Plans from './components/Plans';
+import Row from './components/Row';
+import useAuth from '@/hooks/useAuth';
+import Modal from './components/Modal';
+import { useEffect, useState } from 'react';
+import requests from './utils/requests';
 
-const Home = () => {
+export default function Home() {
+
+  const { loading } = useAuth()
+
+  const subscription = false
+
+  if (loading || subscription === null) return null
+
+  if (!subscription) return <Plans />
+
   return (
     <RecoilRoot>
-      <PageContent />
+      <Child />
     </RecoilRoot>
   )
 }
 
-export default Home
+const Child = () => {
+  const showModal = useRecoilValue(modalState)
+
+  const [movies, setMovies] = useState({
+    netflixOriginals: [],
+    trendingNow: [],
+    topRated: [],
+    actionMovies: [],
+    comedyMovies: [],
+    horrorMovies: [],
+    romanceMovies: [],
+    documentaries: [],
+  })
+
+  useEffect(() => {
+    const movieRequests = Object.values(requests);
+
+    Promise.all(
+      movieRequests.map((url) =>
+        fetch(url, {
+          cache: 'no-store',
+        }).then((response) => {
+          if (!response.ok) {
+            throw new Error(`Request to ${url} failed with status ${response.status}`);
+          }
+          return response.json();
+        })
+      )
+    )
+      .then((responses) => {
+        responses.forEach((response, index) => {
+          setMovies((prevMovies) => ({
+            ...prevMovies,
+            [Object.keys(movies)[index]]: response.results,
+          }));
+        });
+      })
+      .catch((error) => {
+        console.error("Error making requests:", error);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className='relative h-screen bg-gradient-to-b lg:h-[200vh]'>
+      <Header />
+      <main className='relative pl-4 pb-6 lg:space-y-12 lg:pl-16'>
+        <Banner netflixOriginals={movies.netflixOriginals} />
+        <section className="md:space-y-24">
+          <Row title="Trending Now" movies={movies.trendingNow} />
+          <Row title="Top Rated" movies={movies.topRated} />
+          <Row title="Action Thrillers" movies={movies.actionMovies} />
+          {/* My List */}
+          {/* {list.length > 0 && <Row title="My List" movies={list} />} */}
+
+          <Row title="Comedies" movies={movies.comedyMovies} />
+          <Row title="Scary Movies" movies={movies.horrorMovies} />
+          <Row title="Romance Movies" movies={movies.romanceMovies} />
+          <Row title="Documentaries" movies={movies.documentaries} />
+        </section>
+      </main>
+      {showModal && <Modal />}
+    </div>
+  )
+}
